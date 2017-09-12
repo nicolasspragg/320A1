d4d1c6b1bf2417b7a8c00a7b453b6dae8139569d
nicolasspragg <nicolasspragg@gmail.com>
2017-09-11 20:24:04 -0400

    loads first list

--- CMPE320_A1/insultgenerator_12ns73.cpp
+++ CMPE320_A1/insultgenerator_12ns73.cpp
@@ -1,17 +1,48 @@
 #include "insultgenerator_12ns73.h"
 
- //Created by Nicolas for CMPE 320
+//Created by Nicolas for CMPE 320
 //program makes a set of random, unique insults
 #include <iostream>
 #include<fstream>
+#include<string>
+#include <sstream>
+#include<vector>
 using namespace std;
 
 
 InsultGenerator::InsultGenerator() {}
 
+string InsultGenerator::talkToMe()
+{
+	string str= "hello";
+	return str;
+}
+
 // load in insults into vectors 
 void InsultGenerator::initialize() {
-	
+	string line;
+	string t1;
+	string t2;
+	string t3;
+
+	ifstream txtFile("InsultsSource.txt");
+	if (txtFile.fail()) {
+		throw FileException("Wrong file");
+		return;
+	}
+
+	while ((txtFile >> t1) && (txtFile >> t2) && (txtFile >> t3)) {
+	listOfInsults1.push_back(t1);
+	listOfInsults2.push_back(t2);
+	listOfInsults3.push_back(t3);
+	}
+
+
+	for (size_t i = 0; i < listOfInsults1.size(); i++)
+	{
+		cout << listOfInsults1[i] << ' ';
+	}
+
 
 }
 
@@ -19,13 +50,36 @@ void InsultGenerator::initialize() {
 
 
 
+vector<string> InsultGenerator::generate(int n)
+{
+	return vector<string>();
+}
 
+vector<string> InsultGenerator::generateAndSave(string str, int n)
+{
+	return vector<string>();
+}
 
+int InsultGenerator::randomNum()
+{
+	return 0;
+}
 
+NumInsultsOutOfBounds::NumInsultsOutOfBounds(const string &m) : message(m) {};
+FileException::FileException(const string &m) : message(m) {};
+string NumInsultsOutOfBounds::what(){
+	return message;
+}
+string FileException::what() {
+	return message;
+}
 
+// testing 
+int main() {
+	InsultGenerator ig;
 
+	ig.initialize();
 
-
-
-
-InsultGenerator::~InsultGenerator(){}
+	cin.get();
+	return 0;
+}
--- CMPE320_A1/insultgenerator_12ns73.h
+++ CMPE320_A1/insultgenerator_12ns73.h
@@ -1,42 +1,48 @@
 #pragma once
+
 #include <vector>
 #include <set>
-class insultgenerator_12ns73
-{
-public:
-	insultgenerator_12ns73();
-	
-	~insultgenerator_12ns73();
-};
+#include<fstream>
+
+#include <sstream>
+
+
+using namespace std;
 
 class InsultGenerator
-{ public:
-	InsultGenerator();
-	string talkToMe();
-	void initialize();
-	vector<string> generate(int n);
-	vector<string> generateAndSave(string str, int n);
+{ 
+public:
+
+		InsultGenerator();
+		string talkToMe();
+		void initialize();
+		vector<string> generate(const int n);
+		vector<string> generateAndSave(const string str, const int n);
 private:
-	int randomNum();
-	vector<string> list1;
-	vector<string> list2;
-	vector<string> list2;
-	set<string> setOfInsults;
+
+		int randomNum();
+		vector<string> listOfInsults1;
+		vector<string> listOfInsults2;
+		vector<string> listOfInsults3;
+		set<string> setOfInsults;
+		
 
 };
 
 class FileException {
 public:
-	FileException(string str) { message = str; }
-	string what() { return message; }
+	FileException(const string&);
+	string what();
 private:
 	string message;
 };
 
 class NumInsultsOutOfBounds {
 public:
-	NumInsultsOutOfBounds(string str) { message = str; }
-	string what() { return message; }
+	NumInsultsOutOfBounds(const string&);
+	string what();
 private:
 	string message;
 };
+
+
